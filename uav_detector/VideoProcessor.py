bb8d562223b70b8f41d000ad5f3567a36da4cc10
Maly <swampinhabitant@icloud.com>
2024-06-16 19:26:42 +0300

    files_new

--- uav_detector/VideoProcessor.py
+++ uav_detector/VideoProcessor.py
@@ -1,6 +1,5 @@
 import os
 import cv2
-import torch
 import pandas as pd
 from tqdm import tqdm
 from ultralytics import YOLO
@@ -9,16 +8,18 @@ import pillow_heif
 import numpy as np
 
 class MediaProcessor:
-    def __init__(self, output_folder, model_path, confidence_threshold=0.25, batch_size=16):
-        self.device = 'cuda' if torch.cuda.is_available() else 'cpu'
+    def __init__(self, output_folder, model_path, metadata_path, confidence_threshold=0.25, batch_size=16):
         self.output_folder = output_folder
-        self.metadata_folder = 'uav_detector/metadata' 
-        self.model = YOLO(model_path).to(self.device)
+        self.metadata_folder = metadata_path
+        os.makedirs(self.metadata_folder, exist_ok=True)
+        os.makedirs(self.output_folder, exist_ok=True)
+        self.model = YOLO(model_path)
         self.confidence_threshold = confidence_threshold
         self.classes = self.model.names
         self.batch_size = batch_size
 
     def load_image(self, path):
+        # Загрузка изображения с учетом формата файла
         if path.lower().endswith('.heic'):
             heif_file = pillow_heif.open_heif(path)
             image = Image.frombytes(
@@ -33,76 +34,66 @@ class MediaProcessor:
         else:
             return cv2.imread(path)
 
-    def get_Boxes_and_Tables(self, pics):
+    def get_boxes_and_tables(self, pics):
+        # Извлечение боксов и таблиц из изображений
         annotated_images = []
         tables = []
+
         for pic in pics:
-            # Фильтруем результаты на основе порога уверенности
-            #high_conf_indices = pic.boxes.conf > confidence_threshold
-
-            #filtered_boxes = pic.boxes[high_conf_indices]
-
-            # Обновляем данные в объекте `Boxes`
-            #pic.boxes = filtered_boxes
-            
-            # Создаем DataFrame из отфильтрованных данных
+            # Создаем DataFrame из данных боксов
             data_dict = {
                 'class': pic.boxes.cls.cpu().numpy(),
-                'confidence': pic.boxes.conf.cpu().numpy(),
                 'x_center': pic.boxes.xywh[:, 0].cpu().numpy(),
                 'y_center': pic.boxes.xywh[:, 1].cpu().numpy(),
                 'width': pic.boxes.xywh[:, 2].cpu().numpy(),
                 'height': pic.boxes.xywh[:, 3].cpu().numpy()
             }
             df = pd.DataFrame(data_dict)
+            df['class'] = df['class'].astype(int)
             tables.append(df)
 
             # Используем метод plot для получения изображения с рамками
             annotated_image = pic.plot()
             annotated_images.append(annotated_image)
 
-
-            # Поскольку OpenCV загружает изображения в формате BGR, преобразуем их в формат RGB для корректного отображения
-            #annotated_image = cv2.cvtColor(annotated_image, cv2.COLOR_BGR2RGB)
         return annotated_images, tables
 
-    def save_pics(self, images, to_process_names):
-        os.makedirs(self.output_folder, exist_ok=True)
-
+    def save_pics(self, images, to_process_names, tables):
+        # Сохранение изображений и метаданных
         save_paths = []
-        # Сохраняем каждое изображение в отдельный файл
-        for img_array, name_inp in zip(images, to_process_names):
+
+        for img_array, name_inp, table in zip(images, to_process_names, tables):
             img_array = cv2.cvtColor(img_array, cv2.COLOR_BGR2RGB)
-            # Формируем новое имя
             name_out = os.path.splitext(os.path.basename(name_inp))[0]
 
             img = Image.fromarray(img_array)
-            # Создаем корректный путь к файлу
             output_path = os.path.join(self.output_folder, f'{name_out}.png')
             img.save(output_path)
+            output_path_metadata = os.path.join(self.metadata_folder, f'{name_out}.txt')
+            table.to_csv(output_path_metadata, sep='\t', index=False)
             save_paths.append(output_path)
+
         return save_paths
 
     def process_images(self, to_process_names):
-        #results = self.model(images, verbose=False, save_dir='processed_files', save=True)
-        data = self.model(to_process_names, conf = self.confidence_threshold, batch=self.batch_size)
-        pics, tables = self.get_Boxes_and_Tables(data)
-        save_paths = self.save_pics(pics, to_process_names)
-
-        return save_paths, tables
+        # Обработка изображений
+        data = self.model(to_process_names, conf=self.confidence_threshold, batch=self.batch_size)
+        pics, tables = self.get_boxes_and_tables(data)
+        save_paths = self.save_pics(pics, to_process_names, tables)
+        return save_paths
 
     def process_single_video(self, video_path):
+        # Обработка одного видео
         cap = cv2.VideoCapture(video_path)
         output_video_path = os.path.join(self.output_folder, os.path.basename(video_path))
-        fourcc = cv2.VideoWriter_fourcc(*'avc1')#*'avc1')
+        fourcc = cv2.VideoWriter_fourcc(*'mp4v')
         fps = cap.get(cv2.CAP_PROP_FPS)
-        out = cv2.VideoWriter(output_video_path, fourcc, fps, (int(cap.get(3)), int(cap.get(4))))
+        out = cv2.VideoWriter("tmp.mp4", fourcc, fps, (int(cap.get(3)), int(cap.get(4))))
 
         total_frames = int(cap.get(cv2.CAP_PROP_FRAME_COUNT))
         frames = []
 
         frame_num = 0
-        # Инициализация пустого тензора для хранения результатов
         all_classes = []
         all_confidences = []
         all_x_centers = []
@@ -121,7 +112,7 @@ class MediaProcessor:
                 frame_num += 1
 
                 if len(frames) == self.batch_size or frame_num == total_frames:
-                    results = self.model(frames, verbose=False, conf=0.25)
+                    results = self.model(frames, verbose=False, conf=self.confidence_threshold)
 
                     for i, result in enumerate(results):
                         num_detections = len(result.boxes.cls)
@@ -132,22 +123,20 @@ class MediaProcessor:
                             all_y_centers.extend(result.boxes.xywh[:, 1].cpu().tolist())
                             all_widths.extend(result.boxes.xywh[:, 2].cpu().tolist())
                             all_heights.extend(result.boxes.xywh[:, 3].cpu().tolist())
-                            frame_nums.extend([frame_num + i] * num_detections)
+                            frame_nums.extend([frame_num - self.batch_size + i] * num_detections)
 
-                        annotated_frame = result.plot()  # Получаем кадр с нанесенными рамками
+                        annotated_frame = result.plot()
                         out.write(annotated_frame)
 
                     frames = []
                     pbar.update(self.batch_size)
-                
+
         cap.release()
         out.release()
         cv2.destroyAllWindows()
+        os.system('ffmpeg -i {} -vcodec libx264 {}'.format('tmp.mp4', output_video_path))
+        os.remove('tmp.mp4')
 
-        #print(len(frame_nums), len(all_classes), len(all_x_centers), len(all_y_centers), len(all_widths), len(all_heights))
-        # Создание DataFrame
-
-        # Перевод тензоров на CPU и преобразование в списки
         data = {
             'frame_num': frame_nums,
             'class': all_classes,
@@ -158,12 +147,14 @@ class MediaProcessor:
         }
 
         tab = pd.DataFrame(data)
-        tab['class'] = tab['class'].astype(int)#.map(self.classes)
-        os.makedirs(self.metadata_folder, exist_ok=True)
-        tab.to_csv(os.path.join(self.metadata_folder, f'{os.path.basename(video_path)}.csv'))
+        tab['class'] = tab['class'].astype(int)
+        tab['time'] = (tab['frame_num'] / fps).round(3).astype(float)
+        tab.to_csv(os.path.join(self.metadata_folder, f'{os.path.splitext(os.path.basename(video_path))[0]}.csv'), sep=';')
+
         return output_video_path
 
     def process_videos(self, video_paths):
+        # Обработка списка видео
         vids = []
         for video_path in video_paths:
             output_video_path = self.process_single_video(video_path)
@@ -171,6 +162,7 @@ class MediaProcessor:
         return vids
 
 def process_media(input_paths, processor):
+    # Определение типов файлов и их обработка
     image_extensions = ('.jpg', '.jpeg', '.png', '.bmp', '.tiff', '.heic', '.heif', '.webp')
     video_extensions = ('.mp4', '.avi', '.mov', '.mkv', '.flv', '.wmv', '.webm')
 
@@ -180,8 +172,14 @@ def process_media(input_paths, processor):
     img_save_paths_list, vid_save_paths_list = [], []
 
     if image_paths:
-        img_save_paths_list, tables = processor.process_images(image_paths)
+        img_save_paths_list = processor.process_images(image_paths)
     if video_paths:
         vid_save_paths_list = processor.process_videos(video_paths)
-    print(vid_save_paths_list)
+
     return img_save_paths_list, vid_save_paths_list
+
+# Пример использования
+if __name__ == "__main__":
+    input_paths = ["example1.jpg", "example2.heic", "example_video.mp4"]
+    processor = MediaProcessor(output_folder="output", model_path="model.pt", metadata_path="metadata")
+    process_media(input_paths, processor)
